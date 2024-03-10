1521bda89dfa40f4c54b0b759b8200b53954741e
Snowil-tuscano <103951058+Snowil-tuscano@users.noreply.github.com>
2024-03-10 20:25:32 +0530

    evening update

--- employee.py
+++ employee.py
@@ -17,7 +17,7 @@ class emplyee :
         # ................row1...........
         lbl_code = Label(Frame1,text="Employee code",font=("times new roman",18,),bg="white",fg="black").place(x=10,y=60)
         txt_code = Entry(Frame1,font=("times new roman",18,),bg="white",fg="black").place(x=200,y=60,width=200)
-        lbl_code = Button(Frame1,text="Search",font=("times new roman",14),bg="white",fg="black",relief=RAISED).place(x=420,y=60)
+        lbl_code = Button(Frame1,text="Search",font=("times new roman",14),bg="#f0f0f0",fg="black",relief=SOLID).place(x=420,y=60)
 
 
 
@@ -78,17 +78,30 @@ class emplyee :
         lbl_code = Label(Frame2,text="Base Pay",font=("times new roman",18,),bg="white",fg="black").place(x=5,y=60)
         txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=120,y=60,width=90)
         
-        lbl_code = Label(Frame2,text="Present Days",font=("times new roman",18,),bg="white",fg="black").place(x=20,y=60)
+        lbl_code = Label(Frame2,text="Present Days",font=("times new roman",18,),bg="white",fg="black").place(x=230,y=60)
         txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=370,y=60,width=90)
         
-       # .............row...........
-        lbl_code = Label(Frame2,text="Base Pay",font=("times new roman",18,),bg="white",fg="black").place(x=5,y=60)
-        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=120,y=60,width=90)
+       # .............row2...........
+        lbl_code = Label(Frame2,text="Medical",font=("times new roman",18,),bg="white",fg="black").place(x=5,y=120)
+        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=120,y=120,width=90)
         
-        lbl_code = Label(Frame2,text="Present Days",font=("times new roman",18,),bg="white",fg="black").place(x=20,y=60)
-        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=370,y=60,width=90)
-        # lbl_code = Label(Frame2,text="Base Pay",font=("times new roman",18,),bg="white",fg="black").place(x=340,y=60)
-        # txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=250,y=60,width=80)
+        lbl_code = Label(Frame2,text="Convinence",font=("times new roman",18,),bg="white",fg="black").place(x=230,y=120)
+        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=370,y=120,width=90)
+       
+        # .............row3...........
+        lbl_code = Label(Frame2,text="P.F",font=("times new roman",18,),bg="white",fg="black").place(x=5,y=180)
+        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=120,y=180,width=90)
+
+        lbl_code = Label(Frame2,text="Net Salary",font=("times new roman",18,),bg="white",fg="black").place(x=230,y=180)
+        txt_code = Entry(Frame2,font=("times new roman",18),bg="white",fg="black").place(x=370,y=180,width=90)
+        
+        # ..............adding buttons ..............
+        lbl_code = Button(Frame2,text="Calculate",font=("times new roman",14),bg="#f0f0f0",fg="black",relief=SOLID).place(x=180,y=240)
+        lbl_code = Button(Frame2,text="Save",font=("times new roman",14),bg="#f0f0f0",fg="black",relief=SOLID,).place(x=280,y=240)
+        lbl_code = Button(Frame2,text="Clear",font=("times new roman",14),bg="#f0f0f0",fg="black",relief=SOLID).place(x=350,y=240)
+        # lbl_code = Button(Frame2,text="Print",font=("times new roman",14),bg="#f0f0f0",fg="black",relief=SOLID).place(x=360,y=240)
+
+       
 
 
         
@@ -96,6 +109,81 @@ class emplyee :
         # .....................Frame3.................
         Frame3 = Frame(self.root,bd=4,relief=RIDGE)
         Frame3.place(x=770,y=370,width=490,height=270)
+        Frame3.config(bg="white")
+        
+        
+        #.....calculator frame 
+        cal_frame =Frame(Frame3,bg="white",bd=2,relief=RIDGE)
+        cal_frame.place(x=5,y=5,width=216,height=255)
+        
+        self.var_txt=StringVar()
+        self.var_operator=''
+        def btn_click(num):
+          self.var_operator+=str(num)
+          self.var_txt.set(self.var_operator)
+          
+          
+        def result():
+          res=str(eval(self.var_operator))
+          self.var_txt.set(res)
+          self.var_operator=''
+          
+        def clr():
+          self.var_operator=''
+
+          self.var_txt.set(self.var_operator)
+
+          
+
+          
+          
+        title4= Entry(cal_frame,text="Calculator",textvariable=self.var_txt,font=("times new roman",20,),bg="lightgray",fg="black").place(x=0,y=0,relwidth=1)
+
+          
+        # .................calculator row 1 ...............
+        btn7= Button(cal_frame,text="7",command=lambda:btn_click(7) ,font=("times new roman ",15,"bold")).place(x=2,y=42,width=50,height=40)
+        btn8= Button(cal_frame,text="8",command=lambda:btn_click(8),font=("times new roman ",15,"bold")).place(x=55,y=42,width=50,height=40)
+        btn9= Button(cal_frame,text="9",command=lambda:btn_click(9),font=("times new roman ",15,"bold")).place(x=108,y=42,width=50,height=40)
+        btn_div= Button(cal_frame,text="/",command=lambda:btn_click('/'),font=("times new roman ",15,"bold")).place(x=160,y=42,width=50,height=40)
+        
+        
+        # .................calculator row 2 ...............
+        btn4= Button(cal_frame,text="4",command=lambda:btn_click(4),font=("times new roman ",15,"bold")).place(x=2,y=86,width=50,height=40)
+        btn5= Button(cal_frame,text="5",command=lambda:btn_click(5), font=("times new roman ",15,"bold")).place(x=55,y=86,width=50,height=40)
+        btn6= Button(cal_frame,text="6", command=lambda:btn_click(6), font=("times new roman ",15,"bold")).place(x=108,y=86,width=50,height=40)
+        btn_mul= Button(cal_frame,text="*",command=lambda:btn_click('*'), font=("times new roman ",15,"bold")).place(x=160,y=86,width=50,height=40)
+        
+        
+        # # .................calculator row 3 ...............
+        btn1= Button(cal_frame,text="1",command=lambda:btn_click(1),font=("times new roman ",15,"bold")).place(x=2,y=130,width=50,height=40)
+        btn2= Button(cal_frame,text="2",command=lambda:btn_click(2),font=("times new roman ",15,"bold")).place(x=55,y=130,width=50,height=40)
+        btn3= Button(cal_frame,text="3",command=lambda:btn_click(3),font=("times new roman ",15,"bold")).place(x=108,y=130,width=50,height=40)
+        btn_plus= Button(cal_frame,text="+",command=lambda:btn_click('+'),font=("times new roman ",15,"bold")).place(x=160,y=130,width=50,height=40)
+ 
+        # # .................calculator row 4 ...............
+        btn0= Button(cal_frame,text="0",command=lambda:btn_click(0),font=("times new roman ",15,"bold")).place(x=2,y=174,width=50,height=40)
+        btn_neg= Button(cal_frame,text="-",command=lambda:btn_click('-'),font=("times new roman ",15,"bold")).place(x=55,y=174,width=50,height=40)
+        btn_dot= Button(cal_frame,text=".",command=lambda:btn_click('.'),font=("times new roman ",15,"bold")).place(x=108,y=174,width=50,height=40)
+        btn_equal= Button(cal_frame,text="=",command=lambda:result(),font=("times new roman ",15,"bold")).place(x=160,y=174,width=50,height=40)
+        
+        
+        btn_clr = Button(cal_frame,text="Clear",command=lambda:clr(),font=("times new roman ",15,"bold")).place(x=80,y=215,width=60,height=35)
+
+        sal_frame =Frame(Frame3,bg="white",bd=2,relief=RIDGE)
+        sal_frame.place(x=225,y=5,width=253,height=255)   
+        title5= Label(sal_frame,text="Salary Slip",font=("times new roman",20,),bg="lightgray",fg="black").place(x=0,y=0,relwidth=1)
+ 
+        sal_frame2 =Frame(sal_frame,bg="white",bd=2,relief=RIDGE)
+        sal_frame2.place(x=0,y=32,relwidth=1,height=220)   
+        
+        scroll_y= Scrollbar(sal_frame2,orient=VERTICAL)
+        scroll_y.pack(fill=Y,side=RIGHT)
+        
+
+        
+        
+
+        
         
 root = Tk()
 obj = emplyee(root)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-lightgray
+btn1= Button(cal_frame,text="1",font=("times new roman ",15,"bold")).place(x=2,y=126,width=50,height=40)
+        btn1= Button(cal_frame,text="2",font=("times new roman ",15,"bold")).place(x=55,y=126,width=50,height=40)
+        btn1= Button(cal_frame,text="3",font=("times new roman ",15,"bold")).place(x=108,y=126,width=50,height=40)
+        btn1= Button(cal_frame,text="+",font=("times new roman ",15,"bold")).place(x=160,y=126,width=50,height=40)
